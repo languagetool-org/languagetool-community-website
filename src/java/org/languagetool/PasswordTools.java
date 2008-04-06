4076ab903bc1e90e20e602c03ff4bc30936d7392
Daniel Naber <naber@danielnaber.de>
2008-04-06 18:46:01 +0000

    add javadoc

--- src/java/org/languagetool/PasswordTools.java
+++ src/java/org/languagetool/PasswordTools.java
@@ -6,20 +6,37 @@ package org.languagetool;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Utility methods for hashing passwords.
+ * 
+ * @author Daniel Naber
+ */
 public class PasswordTools {
   
+  /**
+   * Delimiter char between plain text salt and hashed password.
+   */
   private static final String SALT_DELIMITER = "-";
 
   private PasswordTools() {
     // static tools, no public constructor
   }
   
+  /**
+   * Calculate a hash of the given string and prepend a
+   * random salt.
+   */
   public static String hash(String password) {
+    // the salt hampers the use of attacks that use 
+    // pre-computed hash values ("rainbow table"):
     String randomSalt = Math.random() + "";
     randomSalt += "." + Math.random() + "";
     return hash(password, randomSalt);
   }
-  
+
+  /**
+   * Calculate a hash of the given string, using the given salt.
+   */
   public static String hash(String password, String salt) {
     if (password == null || password.equals("")) {
       throw new IllegalArgumentException("password may not be null or empty");
@@ -29,6 +46,14 @@ public class PasswordTools {
     return hash;
   }
 
+  /**
+   * Checks of a user-provided password is the same as the stored
+   * password (actually checks if the hashes of both are the same).
+   * 
+   * @param userPassword the plain test password
+   * @param hashPassword the hashed password with a prepended salt
+   * @return true if the password matches the hashed password
+   */
   public static boolean checkPassword(String userPassword, String hashPassword) {
     String parts[] = hashPassword.split(SALT_DELIMITER);
     if (parts.length != 2) {
@@ -42,6 +67,9 @@ public class PasswordTools {
     return false;
   }
   
+  /**
+   * Returns a hex-encoded MD5 sum of the givens string.
+   */
   public static String hexMD5(String s) {
     try {
       MessageDigest md = MessageDigest.getInstance("MD5");
@@ -51,7 +79,10 @@ public class PasswordTools {
     }
   }
 
-  public static String binaryToHex(byte[] array) {
+  /**
+   * Returns a hex representation of the given binary input.
+   */
+  private static String binaryToHex(byte[] array) {
     StringBuffer sb = new StringBuffer();
     for (int i = 0; i < array.length; ++i) {
         sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
