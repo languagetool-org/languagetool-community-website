f7b8457a72c5c09894eb0e81f5d3df47ead68c60
Daniel Naber <naber@danielnaber.de>
2011-11-09 23:31:01 +0000

    use helper webapp to convert MediaWiki syntax to plain text (due to problems with Sweble and LT running in the same context)

--- grails-app/controllers/org/languagetool/WikiCheckController.groovy
+++ grails-app/controllers/org/languagetool/WikiCheckController.groovy
@@ -21,26 +21,50 @@ package org.languagetool
 
 import org.languagetool.dev.wikipedia.WikipediaQuickCheck
 import org.languagetool.dev.wikipedia.WikipediaQuickCheckResult
+import org.apache.commons.io.IOUtils
 
 class WikiCheckController extends BaseController {
   
+  String CONVERT_URL_PREFIX = "http://community.languagetool.org/wikipediatotext/wikiSyntaxConverter/convert?url="
+    
   def index = {
     if (params.url) {
       log.info("WikiCheck: " + params.url)
       WikipediaQuickCheck checker = new WikipediaQuickCheck()
       String langCode = params.url.substring("http://".length(), "http://xx".length())
+      // TODO: remove this restriction
       if (langCode != 'de') {
         throw new Exception("Sorry, only 'de' (German) is supported for now (your language was: '${langCode}')")
       }
-      String plainText = checker.getMediaWikiContent(new URL(params.url))
+      URL plainTextUrl = new URL(CONVERT_URL_PREFIX + params.url)
+      String plainText = download(plainTextUrl)
+      if (plainText == '') {
+        throw new Exception("No page content found at the given URL")
+      }
       Language language = Language.GERMAN
       WikipediaQuickCheckResult result = checker.checkPage(plainText, language)
       params.lang = result.getLanguageCode()
       [result: result, matches: result.getRuleMatches(), textToCheck: result.getText(),
               lang: result.getLanguageCode(),
-              url: params.url, disabledRuleIds: checker.getDisabledRuleIds()]
+              url: params.url, disabledRuleIds: checker.getDisabledRuleIds(),
+              plainText: plainText]
     } else {
       []
+    }
+  }
+    
+  private String download(final URL url) throws IOException {
+    final HttpURLConnection connection = (HttpURLConnection)url.openConnection()
+    if (connection.getResponseCode() != 200) {
+      throw new IOException("Server error for " +  url + ", code: " + connection.getResponseCode())
+    }
+    final InputStream is = connection.getInputStream()
+    try {
+      StringWriter writer = new StringWriter()
+      IOUtils.copy(is, writer, "utf-8")
+      return writer.toString()
+    } finally {
+      is.close()
     }
   }
 
