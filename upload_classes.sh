d08e30a8e36a929680a9be101e4bf86c6686bad8
Daniel Naber <naber@danielnaber.de>
2011-11-19 00:04:10 +0000

    helper script: restart tomcat after upload of new version

--- upload_classes.sh
+++ upload_classes.sh
@@ -4,4 +4,4 @@
 grails war
 scp -i /home/dnaber/.ssh/openthesaurus target/ltcommunity-0.1.war languagetool@83.169.5.38:/tmp
 ssh -i /home/dnaber/.ssh/openthesaurus languagetool@83.169.5.38 unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
-echo "Now restart Tomcat yourself"
+ssh -i /home/dnaber/.ssh/openthesaurus languagetool@83.169.5.38 ./restart.sh
