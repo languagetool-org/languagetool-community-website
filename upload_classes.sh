57bbf106ceb33b2c50b799694a523f7681877700
Daniel Naber <naber@danielnaber.de>
2012-08-20 16:54:33 +0000

    update helper script to use new server

--- upload_classes.sh
+++ upload_classes.sh
@@ -1,7 +1,12 @@
 #/bin/sh
 # small helper script for deployment
 
+#old:
+#SERVER=83.169.5.38
+#new (August 2012):
+SERVER=176.28.12.50
+
 grails war
-scp -i /home/dnaber/.ssh/openthesaurus target/ltcommunity-0.1.war languagetool@83.169.5.38:/tmp
-ssh -i /home/dnaber/.ssh/openthesaurus languagetool@83.169.5.38 unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
-ssh -i /home/dnaber/.ssh/openthesaurus languagetool@83.169.5.38 ./restart.sh
+scp -i /home/dnaber/.ssh/openthesaurus target/ltcommunity-0.1.war languagetool@$SERVER:/tmp
+ssh -i /home/dnaber/.ssh/openthesaurus languagetool@$SERVER unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
+ssh -i /home/dnaber/.ssh/openthesaurus languagetool@$SERVER ./restart.sh
