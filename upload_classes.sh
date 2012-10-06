d0f4f5339e037bb07c7432aebdc324ce2c27f364
Daniel Naber <naber@danielnaber.de>
2012-10-06 11:37:09 +0000

    make the admin log in manually (I get the old JRE otherwise and the app won't deploy)

--- upload_classes.sh
+++ upload_classes.sh
@@ -9,4 +9,4 @@ SERVER=176.28.12.50
 grails war
 scp -i /home/dnaber/.ssh/openthesaurus target/ltcommunity-0.1.war languagetool@$SERVER:/tmp
 ssh -i /home/dnaber/.ssh/openthesaurus languagetool@$SERVER unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
-ssh -i /home/dnaber/.ssh/openthesaurus languagetool@$SERVER ./restart.sh
+echo "Now log in to the server and call ./restart.sh to activate the update"
