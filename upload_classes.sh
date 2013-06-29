63a2aab027a059b79721480e8cbaef8b65874875
Daniel Naber <naber@danielnaber.de>
2013-06-29 13:08:57 +0000

    update message

--- upload_classes.sh
+++ upload_classes.sh
@@ -1,12 +1,9 @@
 #/bin/sh
 # small helper script for deployment
 
-#old:
-#SERVER=83.169.5.38
-#new (August 2012):
 SERVER=176.28.12.50
 
 grails war
 scp -i /home/dnaber/.ssh/openthesaurus target/ltcommunity-0.1.war languagetool@$SERVER:/tmp
 ssh -i /home/dnaber/.ssh/openthesaurus languagetool@$SERVER unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
-echo "Now log in to the server and call ./restart.sh to activate the update"
+echo "Now log in to the server and call ./restart-tomcat.sh to activate the update"
