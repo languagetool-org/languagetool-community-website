123a298f7c81266513ddaf34d752c69b3b0e90c1
Daniel Naber <naber@danielnaber.de>
2013-07-09 07:14:59 +0000

    update script to also upload language-related resources, including *.properties file in META-INF

--- upload_classes.sh
+++ upload_classes.sh
@@ -1,9 +1,23 @@
 #/bin/sh
 # small helper script for deployment
 
-SERVER=176.28.12.50
+echo ""
+echo "###"
+echo "### Admin only - you will need the server password to deploy the code ###"
+echo "### This will deploy the code for http://community.languagetool.org ###"
+echo "###"
+echo ""
+sleep 1
 
 grails war
-scp -i /home/dnaber/.ssh/openthesaurus target/ltcommunity-0.1.war languagetool@$SERVER:/tmp
-ssh -i /home/dnaber/.ssh/openthesaurus languagetool@$SERVER unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
+
+scp target/ltcommunity-0.1.war languagetool@languagetool.org:/tmp
+ssh languagetool@languagetool.org unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
+
+cd lib
+zip -r /tmp/languagetool-not-in-jars.jar META-INF/ org/
+scp /tmp/languagetool-not-in-jars.jar languagetool@languagetool.org:tomcat/webapps/ROOT/WEB-INF/lib/
+rm /tmp/languagetool-not-in-jars.jar
+cd -
+
 echo "Now log in to the server and call ./restart-tomcat.sh to activate the update"
