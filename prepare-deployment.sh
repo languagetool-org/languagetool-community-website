3f18973aab7307be8953f2ae4af5183a5a73b637
Daniel Naber <naber@danielnaber.de>
2013-11-25 17:34:44 +0100

    dependencies are managed by Maven, no need to upload the rules and other language resources explicitly

--- prepare-deployment.sh
+++ prepare-deployment.sh
@@ -3,8 +3,8 @@
 
 echo ""
 echo "###"
-echo "### Admin only - you will need the server password to deploy the code ###"
 echo "### This will deploy the code for http://community.languagetool.org ###"
+echo "### Admin only - you will need the server password to deploy the code ###"
 echo "###"
 echo ""
 sleep 1
@@ -13,11 +13,5 @@ grails war
 
 scp target/ltcommunity-0.1.war languagetool@languagetool.org:/tmp
 ssh languagetool@languagetool.org unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
-
-cd lib
-zip -r /tmp/languagetool-not-in-jars.jar META-INF/ org/
-scp /tmp/languagetool-not-in-jars.jar languagetool@languagetool.org:tomcat/webapps/ROOT/WEB-INF/lib/
-rm /tmp/languagetool-not-in-jars.jar
-cd -
 
 echo "Now log in to the server and call ./restart-tomcat.sh to activate the update"
