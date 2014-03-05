66a7d58766c3e268fd0ec5e6d8412e017c78942f
Daniel Naber <naber@danielnaber.de>
2014-03-05 13:31:18 +0100

    update instructions in the deploy script

--- prepare-deployment.sh
+++ prepare-deployment.sh
@@ -14,4 +14,7 @@ grails war
 scp target/ltcommunity-0.1.war languagetool@languagetool.org:/tmp
 ssh languagetool@languagetool.org unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
 
-echo "Now log in to the server and call ./restart-tomcat.sh to activate the update"
+echo "Now log in to the server and:"
+echo "-Call sh /home/languagetool/languagetool.org/languagetool-website/deploy-jars.sh"
+echo " (this will place the latest LT snapshot JARs in WEB-INF/lib)"
+echo "-Call ./restart-tomcat.sh to activate the update"
