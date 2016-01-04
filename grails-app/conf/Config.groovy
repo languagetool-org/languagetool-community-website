a3427c2ec66be69876b7d567746399c0f40f138d
Daniel Naber <naber@danielnaber.de>
2016-01-04 09:04:58 +0100

    show Danish as requested; show Catalan again (was lost due to changes in class names and language codes)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -40,8 +40,8 @@ ngramindex = "/home/languagetool/ngram-data"
 max.text.length = 10000     // maximum length of texts to check
 // languages not visible in the user interface (e.g. because they are
 // not properly supported yet):
-hide.languages = ["cs", "ml", "be", "da", "sk", "zh", "ast", "km", "en-GB", "en-US", "en-CA", "en-ZA", "en-NZ", "en-AU",
-        "de-DE", "de-AT", "de-CH", "pt-BR", "pt-PT", "ca-ES-valencia", "ca-ES", "de-DE-x-simple-language"]
+hide.languages = ["cs", "ml", "be", "sk", "zh", "ast", "km", "en-GB", "en-US", "en-CA", "en-ZA", "en-NZ", "en-AU",
+        "de-DE", "de-AT", "de-CH", "pt-BR", "pt-PT", "ca-ES-valencia", "de-DE-x-simple-language"]
 // show these languages directly in the navigation bar (not in the drop down):
 expose.languages = ["en", "fr", "de", "es", "pl", "ca", "br", "nl", "pt", "es", "eo", "ru"]
 maxPatternElements = 5
