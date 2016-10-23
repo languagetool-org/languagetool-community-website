06a7978c16bff6c09aceef158fa5a8fd0f5d9af9
Daniel Naber <naber@danielnaber.de>
2016-10-23 14:33:33 +0200

    be more strict in checking: correction="" will fail if there is a correction

--- grails-app/controllers/org/languagetool/RuleEditorController.groovy
+++ grails-app/controllers/org/languagetool/RuleEditorController.groovy
@@ -194,6 +194,9 @@ class RuleEditorController extends BaseController {
                 def ruleMatch = ruleMatches.get(0)
                 def expectedReplacements = []
                 expectedReplacements.addAll(incorrectExample.corrections)
+                if (expectedReplacements.size() == 1 && expectedReplacements.get(0).isEmpty()) {
+                    expectedReplacements = []
+                }
                 expectedReplacements.sort()
                 if (checkMarker) {
                     int expectedMatchStart = incorrectExample.getExample().indexOf("<marker>")
@@ -216,6 +219,8 @@ class RuleEditorController extends BaseController {
                 def foundReplacements = new ArrayList<>(ruleMatches.get(0).getSuggestedReplacements())
                 foundReplacements.sort()
                 if (expectedReplacements.size() > 0 && !expectedReplacements.get(0).isEmpty() && expectedReplacements != foundReplacements) {
+                    problems.add(message(code: 'ltc.editor.error.wrong.correction', args: [sentence, foundReplacements, expectedReplacements]))
+                } else if (expectedReplacements.size() == 0 && expectedReplacements != foundReplacements) {
                     problems.add(message(code: 'ltc.editor.error.wrong.correction', args: [sentence, foundReplacements, expectedReplacements]))
                 }
             } else {
