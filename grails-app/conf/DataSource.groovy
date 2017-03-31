fd14636b02a94f17051bd928fb84fbb50c409549
Daniel Naber <naber@danielnaber.de>
2017-03-31 18:24:30 +0200

    add option because the driver complained about the time zone being ambiguous

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -18,7 +18,7 @@ environments {
 		dataSource {
 		    driverClassName = "com.mysql.jdbc.Driver"
 			dbCreate = "update" // one of 'create', 'create-drop','update'
-			url = "jdbc:mysql://localhost/ltcommunity?useUnicode=true&characterEncoding=UTF-8"
+			url = "jdbc:mysql://localhost/ltcommunity?useUnicode=true&characterEncoding=UTF-8&useLegacyDatetimeCode=false&serverTimezone=Europe/Berlin"
 		    username = "root"
 		    password = ""
 		}
@@ -33,7 +33,7 @@ environments {
 		dataSource {
 		  driverClassName = "com.mysql.jdbc.Driver"
 		  dbCreate = "update" // one of 'create', 'create-drop','update'
-          url = "jdbc:mysql://localhost/ltcommunity?useUnicode=true&characterEncoding=UTF-8"
+          url = "jdbc:mysql://localhost/ltcommunity?useUnicode=true&characterEncoding=UTF-8&useLegacyDatetimeCode=false&serverTimezone=Europe/Berlin"
           username = ""
           password = ""
 		}
