4e0bfe47c363b10db72b86b99a8a899675d78062
Daniel Naber <naber@danielnaber.de>
2018-09-29 16:05:35 +0200

    update path

--- prepare-deployment.sh
+++ prepare-deployment.sh
@@ -15,5 +15,5 @@ scp -i ~/.ssh/lt2017 target/ltcommunity-0.1.war languagetool@community.languaget
 ssh -i ~/.ssh/lt2017 languagetool@community.languagetool.org unzip -d /home/languagetool/tomcat/webapps/ROOT/ /tmp/ltcommunity-0.1.war
 
 echo "Now log in to the server and call:"
-echo "sh /home/languagetool/languagetool.org/languagetool-website/deploy-jars.sh `date +%Y%m%d` (or with yesterday's date if there's no snapshot for today yet)"
+echo "sh /home/languagetool/deploy-jars.sh `date +%Y%m%d` (or with yesterday's date if there's no snapshot for today yet)"
 echo " (this will place the latest LT snapshot JARs in WEB-INF/lib)"
