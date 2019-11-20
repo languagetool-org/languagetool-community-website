c72a69612c9d34c9d7145b6cd03704b17fd51453
Daniel Naber <naber@danielnaber.de>
2019-11-20 12:53:42 +0100

    make script work again

--- prepare-deployment.sh
+++ prepare-deployment.sh
@@ -11,9 +11,7 @@ sleep 1
 
 grails war
 
-scp -i ~/.ssh/lt2017 target/ltcommunity-0.1.war languagetool@community.languagetool.org:/tmp
-ssh -i ~/.ssh/lt2017 root@community.languagetool.org unzip -d /home/languagetool/webapps/ROOT/ /tmp/ltcommunity-0.1.war
+scp -i ~/.ssh/lt2017 target/ltcommunity-0.1.war languagetool@community:/tmp
+ssh -i ~/.ssh/lt2017 root@community unzip -d /home/languagetool/webapps/ROOT/ /tmp/ltcommunity-0.1.war
 
-echo "Now log in to the server and call:"
-echo "sh /home/languagetool/deploy-jars.sh `date +%Y%m%d` (or with yesterday's date if there's no snapshot for today yet)"
-echo " (this will place the latest LT snapshot JARs in WEB-INF/lib)"
+echo "Now wait for Tomcat to auto-restart (or call 'systemctl restart tomcat9.service' on the server)"
