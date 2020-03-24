928f7c9239f9396f28721bd84ffa0d1cbd9b9de8
Daniel Naber <naber@danielnaber.de>
2020-03-24 16:07:32 +0100

    use 5.0-SNAPSHOT dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -36,7 +36,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        runtime('org.languagetool:language-all:4.9-SNAPSHOT') {
+        runtime('org.languagetool:language-all:5.0-SNAPSHOT') {
             exclude "slf4j-nop"
         }
         runtime 'org.languagetool:languagetool-wikipedia:4.9-SNAPSHOT'
