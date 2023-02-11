9cce22ac4e4e89992c5f99cdc4e97d4eb5eee1b9
Daniel Naber <naber@danielnaber.de>
2023-02-11 22:20:19 +0100

    show only most popular languages in menu

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -46,9 +46,10 @@ max.text.length = 10000     // maximum length of texts to check
 // languages not visible in the user interface (e.g. because they are
 // not properly supported yet):
 hide.languages = ["cs", "ml", "be", "sk", "zh", "ast", "km", "en-GB", "en-US", "en-CA", "en-ZA", "en-NZ", "en-AU",
-        "de-DE", "de-AT", "de-CH", "pt-BR", "pt-PT", "pt-AO", "pt-MZ", "ca-ES-valencia", "de-DE-x-simple-language", "sr-BA", "sr-RS", "sr-ME", "sr-HR", "nl-BE"]
+        "de-DE", "de-AT", "de-CH", "pt-BR", "pt-PT", "pt-AO", "pt-MZ", "ca-ES-valencia", "de-DE-x-simple-language", "sr-BA", "sr-RS", "sr-ME", "sr-HR", "nl-BE",
+        "ca-ES-balear", "es-AR"]
 // show these languages directly in the navigation bar (not in the drop down):
-expose.languages = ["en", "fr", "de", "es", "pl", "ca", "br", "nl", "pt", "es", "eo", "ru"]
+expose.languages = ["en", "fr", "de", "es", "pl", "ca", "nl", "pt", "es", "ru", "uk"]
 maxPatternElements = 5
 // the feed of user-suggested words is password protected:
 suggestion.password = "fixme"
