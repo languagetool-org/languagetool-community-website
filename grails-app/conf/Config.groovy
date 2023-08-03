e4fdddf981e087258bd7d6282939542df7d399a1
Daniel Naber <naber@danielnaber.de>
2023-08-03 14:03:15 +0200

    hide more variants

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -47,7 +47,7 @@ max.text.length = 10000     // maximum length of texts to check
 // not properly supported yet):
 hide.languages = ["cs", "ml", "be", "sk", "zh", "ast", "km", "en-GB", "en-US", "en-CA", "en-ZA", "en-NZ", "en-AU",
         "de-DE", "de-AT", "de-CH", "pt-BR", "pt-PT", "pt-AO", "pt-MZ", "ca-ES-valencia", "de-DE-x-simple-language", "sr-BA", "sr-RS", "sr-ME", "sr-HR", "nl-BE",
-        "ca-ES-balear", "es-AR"]
+        "ca-ES-balear", "es-AR", "fr-BE", "fr-CA", "fr-CH"]
 // show these languages directly in the navigation bar (not in the drop down):
 expose.languages = ["en", "fr", "de", "es", "pl", "ca", "nl", "pt", "es", "ru", "uk"]
 maxPatternElements = 5
